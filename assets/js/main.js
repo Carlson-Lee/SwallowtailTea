725b888a4a439d4f0c0ffac3b1c146133b11a368
Carlson-Lee <zhonj4@student.op.ac.nz>
2022-06-22 01:18:31 +1200

    added app.js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,27 @@
+//const { response } = require("express");
+
 //paragraph change//
-let sales = document.querySelector("")
+let background_banner = document.querySelector(".background_temp");
+
+//document.querySelectorAll("header_link").addEventListener("hover", background_banner(){
+    
+
+//})
+
+fetch('https://api.unsplash.com/search/photos?query=tea-leafs&client_id=c3p_40xZo67k-Vhy73WXyrVOF_dBByjLqznqOtmT9dY')
+.then(response => response.json())
+.then(image => {
+    //let randomNumb = Math.floor(Math.random() * 999);
+    let numb = 1;
+    console.log(randomNumb);
+    console.log(image);
+
+    if(numb === image[0]){
+        let img = document.createElement("div");
+        img.classList.add("unsplash_img")
+        img.innerHTML = image
+    }
+
+    
+})
+background_banner.append(image);
