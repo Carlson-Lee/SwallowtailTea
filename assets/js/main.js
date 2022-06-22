3ebd564ddc7e062f78085bdde2c3e698b33f3765
Carlson-Lee <zhonj4@student.op.ac.nz>
2022-06-22 16:12:32 +1200

    fetching unsplash img as background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,21 @@
-//const { response } = require("express");
-
-//paragraph change//
-let background_banner = document.querySelector(".background_temp");
-
-//document.querySelectorAll("header_link").addEventListener("hover", background_banner(){
-    
-
-//})
-
 fetch('https://api.unsplash.com/search/photos?query=tea-leafs&client_id=c3p_40xZo67k-Vhy73WXyrVOF_dBByjLqznqOtmT9dY')
 .then(response => response.json())
 .then(image => {
-    //let randomNumb = Math.floor(Math.random() * 999);
-    let numb = 1;
+    let randomNumb = Math.floor(Math.random() * 999);
     console.log(randomNumb);
-    console.log(image);
 
-    if(numb === image[0]){
-        let img = document.createElement("div");
-        img.classList.add("unsplash_img")
-        img.innerHTML = image
-    }
+    console.log(image["results"]);
 
-    
+    image["results"].forEach(img => {
+        console.log(img["urls"]);
+
+        function randomCheck(a,b){
+            return randomNumb.join() == img.join();
+        }
+        console.log(randomCheck(a,b));
+
+        document.body.style.backgroundImage = `url(${img.urls.regular})`;
+    })
+
 })
-background_banner.append(image);
+
