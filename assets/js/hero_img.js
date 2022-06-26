7f7465132df3f8f738a8cd8d528c2c73ecfc015c
Carlson-Lee <zhonj4@student.op.ac.nz>
2022-06-26 16:51:04 +1200

    changing if statements and for  loop

--- assets/js/hero_img.js
+++ assets/js/hero_img.js
@@ -6,41 +6,138 @@ let header_link = Array.from(document.querySelectorAll(".header_link"));
 let elements1 = document.querySelectorAll('[data-index]');
 
 
-console.log(header_link);
 
 header_link.forEach(link => {
-    console.log(link);
-    link.addEventListener("mouseenter", mouseEnter);
-    link.addEventListener("mouseleave", mouseLeave);
-    //start of the function//
-    function mouseEnter()
+link.addEventListener("mouseenter", mouseEnter);
+link.addEventListener("mouseleave", mouseLeave);
+});
+function mouseEnter()
     {
-        console.log("mouse entered! ")
-        for(i=0; i<1; i++){
-            if(header_link[i] != slide_hero_image[i] && hero_bg_contents[i]){
-                console.log("not");
+        //console.log(header_link[i],slide_hero_image[i],hero_bg_contents[i]);
+ 
+        for(i=0; i<header_link.length;i++){
+            if(i != 0){
+                console.log("no");
+                if(i = 1)
+                {
+                    console.log("YEs no2");
+                    document.querySelector(".slide_hero_image").classList.add("active");
+                    document.querySelector(".hero_bg_contents").classList.add("active");
+                }
+                if(i = 2)
+                {
+                    document.querySelector(".slide_hero_image").classList.remove("active");
+                    document.querySelector(".hero_bg_contents").classList.remove("active");
+                    document.querySelector(".slide_hero_image").classList.add("active");
+                    document.querySelector(".hero_bg_contents").classList.add("active");
+                }
+                else{
+                    console.log("nested else")
+                    document.querySelector(".slide_hero_image").classList.remove("active");
+                    document.querySelector(".hero_bg_contents").classList.remove("active");
+                }
+                
+                console.log("1st else");
             }
-            else
-            {
-                document.querySelector(".hero_bg_contents").classList.remove("active");
-            }
+
+            //console.log(i);
         }
-        document.querySelector(".slide_hero_image").classList.add("active");
-        document.querySelector(".hero_bg_contents").classList.add("active");
+
+    }    
+
+//(header_link[0] == slide_hero_image[0]) && (slide_hero_image[0] == hero_bg_contents[0]) && (header_link[0] == hero_bg_contents[0])
+//(header_link[i] != slide_hero_image[i]) && (slide_hero_image[i] != hero_bg_contents[i]) && (header_link[i] != hero_bg_contents[i])
+//             if(header_link[1] && slide_hero_image[1] && hero_bg_contents[1])
+//             {
+//                 console.log("enter 1");
+//                 document.querySelector(".hero_bg_contents").classList.remove("active");
+//                 document.querySelector(".slide_hero_image").classList.add("active");
+//                 document.querySelector(".hero_bg_contents").classList.add("active");
+//             }
+ 
+
+//             if(header_link[2] && slide_hero_image[2] && hero_bg_contents[2])
+//             {
+//                 console.log("enter 2");
+//                 document.querySelector(".hero_bg_contents").classList.remove("active");
+//                 document.querySelector(".slide_hero_image").classList.add("active");
+//                 document.querySelector(".hero_bg_contents").classList.add("active");
+//             }
+
+
+
+
+//                 document.querySelector(".hero_bg_contents").classList.remove("active");
+
+
+// }
+
+        
+function mouseLeave(l)
+{
+    console.log("mouse Leave!")
+    document.querySelector(".slide_hero_image").classList.remove("active");
+    document.querySelector(".hero_bg_contents").classList.remove("active");
+}
+
+// switch(m)
+// {
+
+//     case 1:
+//         if(header_link[0] && slide_hero_image[0] && hero_bg_contents[0])
+//         {
+            
+//             document.querySelector(".slide_hero_image").classList.add("active");
+//             document.querySelector(".hero_bg_contents").classList.add("active");
+//         }
+//     break;
+    
+//     case 2:
+//         if(header_link[1] && slide_hero_image[1] && hero_bg_contents[1])
+//         {
+//             console.log("enter 1");
+//             document.querySelector(".hero_bg_contents").classList.remove("active");
+//             document.querySelector(".slide_hero_image").classList.add("active");
+//             document.querySelector(".hero_bg_contents").classList.add("active");
+//         }
+//     break;
+    
+//     case 3:
+//         if(header_link[2] && slide_hero_image[2] && hero_bg_contents[2])
+//         {
+//             console.log("enter 2");
+//             document.querySelector(".hero_bg_contents").classList.remove("active");
+//             document.querySelector(".slide_hero_image").classList.add("active");
+//             document.querySelector(".hero_bg_contents").classList.add("active");
+//         }
+//     break;
+
+//     default:
+//         {
+//             document.querySelector(".hero_bg_contents").classList.remove("active");
+//         }
+// }
+
+
+
+
+
+
+
+
+    //start of the function//
+   
+
+        // document.querySelector(".slide_hero_image").classList.add("active");
+        // document.querySelector(".hero_bg_contents").classList.add("active");
 
         // if(header_link[0] = slide_hero_image[0] && hero_bg_contents[0])
         //     {
         //         document.querySelector(".slide_hero_image").classList.add("active");
         //         document.querySelector(".hero_bg_contents").classList.add("active");
         //     }
-    }
 
-    function mouseLeave()
-    {
-        console.log("mouse Leave!")
-        //document.querySelector(".hero_bg_contents").classList.remove("active");
-    }
-})
+
 
 
 
